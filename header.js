3a76aa35aeca9a53e2b5a3b55f0dd86c774cebd0
ffortin-sci-edu <ffortin.sci.edu@gmail.com>
2022-11-23 15:53:22 +0100

    Adding css and js

---
+++ header.js
@@ -0,0 +1,22 @@
+document.write(`
+<img src="https://raw.githubusercontent.com/ffortin-sci-edu/ffortin-sci-edu.github.io/master/logo_banner.png" alt="logo_banner" align="right" width="400px">
+
+<a href="https://ffortin-sci-edu.github.io/index.html" style="color:black">
+<img src="https://raw.githubusercontent.com/ffortin-sci-edu/ffortin-sci-edu.github.io/master/DLT_cut.png" alt="DLT" width="50" align="left" style="position:relative; color:black; left:10px; top:5px;">
+
+<h1><p style="margin-left:2.5em; margin-top:0.5em">Francis Fortin</p></h1>
+<b><p style="margin-left:5em"> Post-doc Researcher </p></b></a>
+
+<br>
+<hr>
+
+<nav>
+  <ul>
+    <li><a href="https://ffortin-sci-edu.github.io/index.html">Home</a></li>
+    <li><a href="https://ffortin-sci-edu.github.io/CV_FORTIN_FR.pdf" target="_blank" rel="noreferrer noopener">CV</a></li>
+    <li><a href="https://ffortin-sci-edu.github.io/Publications.html">Publications</a></li>
+    <li><a href="https://ffortin-sci-edu.github.io/Enseignement.html">Teachings</a></li>
+    <li><a href="https://ffortin-sci-edu.github.io/misc.html">About</a></li>
+  </ul><hr>
+</nav>
+`);
